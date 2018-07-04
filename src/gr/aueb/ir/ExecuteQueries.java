a27d4a0244ea1dada6cd40c6d03c9f4d210c8438
Nasia (Athanasia) Koutsopoulou <athkoutsop@yahoo.gr>
2018-07-04 21:15:20 +0300

    xd

--- src/gr/aueb/ir/ExecuteQueries.java
+++ src/gr/aueb/ir/ExecuteQueries.java
@@ -16,6 +16,18 @@ public class ExecuteQueries {
     // arraylist that each query is going to be stored at
     private static ArrayList<Query> queries;
 
+    public static void main(String [ ] args) {
+        // read the cran.qry and saves its transformed version to an arraylist of queries
+        readAndTransformQueries("data/cran.qry");
+
+        boolean useRerank = false;
+        // exexutes the queries
+        runQueries(useRerank);
+
+        // modifies the creanqrel file to a form that is accepted to the trec eval
+        ModifyCollection.modifyQrelFile("data/cranqrel");
+    }
+
     public static void readAndTransformQueries(String fileName) {
         try {
             // we use buffereader to read the input file
@@ -62,7 +74,8 @@ public class ExecuteQueries {
         }
     }
 
-    public static void runQueries() {
+    public static void runQueries(boolean reranked) {
+        // we create our solr client
         String solrUrl = "http://localhost:8983/solr";
         HttpSolrClient client = new HttpSolrClient.Builder(solrUrl)
                 .withConnectionTimeout(10000)
@@ -70,6 +83,7 @@ public class ExecuteQueries {
                 .build();
 
         try {
+            // we run our queries one by one and write the results to the queryResults.txt
             int queryCounter = 1;
             Path outputFile = Paths.get("data/queryResults");
             if (Files.exists(outputFile)) {
@@ -80,6 +94,7 @@ public class ExecuteQueries {
                 Files.createFile(outputFile);
             }
 
+            // we simple replace some special chars with space
             PrintWriter writer = new PrintWriter(outputFile.toString(), "UTF-8");
             for (Query q : queries) {
                 String currentQuery = q.getQuery().replace("(", " ")
@@ -93,29 +108,32 @@ public class ExecuteQueries {
                 StringTokenizer tokenizer = new StringTokenizer(currentQuery, " ");
                 String word = tokenizer.nextToken();
 
+                // we create our query param to be input to solr
                 StringBuilder queryWords = new StringBuilder();
-//                queryWordsWIthOr.append(T + word +
-//                                        A + word +
-//                                        B + word +
-//                                        W + word);
                 queryWords.append("text:" + word);
                 while (tokenizer.hasMoreTokens()) {
                     word = tokenizer.nextToken();
-//                    queryWordsWIthOr.append(" OR " + T + word +
-//                                                A + word +
-//                                                B + word +
-//                                                W + word);
                     queryWords.append(" OR text:" + word);
                 }
 
-                StringBuilder reRankWords = constructRerankQuery(currentQuery);
-
                 Map<String, String> queryParamMap = new HashMap<>();
-                queryParamMap.put("q", queryWords.toString());
-                //queryParamMap.put("rq", "{!rerank reRankQuery=$rqq reRankDocs=20 reRankWeight=3}");
-                queryParamMap.put("rqq", reRankWords.toString());
-                queryParamMap.put("fl", "identifier,score");
-                queryParamMap.put("rows", "500");
+
+                // contstructs a rerank query in case its needed
+                if(reranked) {
+                    String reRankWord = maxLengthWordInQuery(currentQuery);
+
+                    queryParamMap.put("q", queryWords.toString());
+                    queryParamMap.put("rq", "{!ltr model=myEfiModel reRankDocs=20 efi.word1=" + reRankWord.trim() +
+                            " efi.word2=" + reRankWord.trim() + "}");
+                    queryParamMap.put("fl", "identifier,score");
+                    queryParamMap.put("rows", "500");
+                }
+                // else we create a simple query
+                else{
+                    queryParamMap.put("q", queryWords.toString());
+                    queryParamMap.put("fl", "identifier,score");
+                    queryParamMap.put("rows", "500");
+                }
 
                 MapSolrParams queryParams = new MapSolrParams(queryParamMap);
 
@@ -123,11 +141,13 @@ public class ExecuteQueries {
 
                 SolrDocumentList documents = response.getResults();
 
+                // we write the retrieved docs to the reults file using a format of "qureyId, iter docId rank score Standar" as needed by treceval, in order to use treceval later for evaluation
                 for (SolrDocument document : documents) {
                     String identifier = document.getFieldValue("identifier").toString()
                             .substring(1, document.getFieldValue("identifier").toString().indexOf("]"));
                     writer.println(queryCounter + " 0 " + identifier + " 1 " + document.getFieldValue("score") + " STANDARD");
                 }
+                // we use query counter to set the queries ids due to error to the cran.qry file
                 queryCounter++;
             }
             writer.close();
@@ -136,13 +156,16 @@ public class ExecuteQueries {
         }
     }
 
-    private static StringBuilder constructRerankQuery(String query) {
-        StringBuilder rerankQuery = new StringBuilder();
-        rerankQuery.append("title:");
+    // returns the word having the longest lenght withing the query passed
+    private static String maxLengthWordInQuery(String query) {
+        String rerankQuery = "";
 
+        // arraylist with elements the words of the query
         ArrayList<String> words = new ArrayList<>();
+        // arraylist with elements the length of the words of the query. "paraller" to the above arraylist
         ArrayList<Integer> wordLenghts = new ArrayList<>();
 
+        // we populate both arraylists
         StringTokenizer tokenizer = new StringTokenizer(query, " ");
         words.add(tokenizer.nextToken());
 
@@ -152,22 +175,19 @@ public class ExecuteQueries {
             wordLenghts.add(words.get(words.size() - 1).length());
         }
 
+        // we create a copy of the arraylist containing the lengths and sort it
         ArrayList<Integer> sortedLengths = new ArrayList<>(wordLenghts);
         Collections.sort(sortedLengths);
 
+        // we get the max lenght of the words
         int counter = sortedLengths.size()-1;
         int max = sortedLengths.get(counter);
 
-        boolean first = true;
+        // we return the worlds with that max lenght
         for(int i = 0; i < wordLenghts.size(); i++){
             if(max == wordLenghts.get(i)){
-                if(first){
-                    rerankQuery.append(words.get(i));
-                    first = false;
-                }
-                else{
-                    rerankQuery.append(" OR title:" +words.get(i));
-                }
+                rerankQuery = words.get(i);
+                break;
             }
         }
         return rerankQuery;
